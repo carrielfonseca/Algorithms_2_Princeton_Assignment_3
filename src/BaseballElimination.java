e730c68eaf2683851005c79f8bcd0a5e4c801d2a
Fabio Carriel Fonseca <ffonseca@modulusdata.com>
2018-01-28 21:22:58 -0500

    returns number of wins

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -44,7 +44,17 @@ public class BaseballElimination {
 	public Iterable<String> teams()   {                             // all teams
 		return teams;
 	}
-//	public              int wins(String team)                      // number of wins for given team
+	
+	public int wins(String team) {                  // number of wins for given team
+		int i = 0;
+		for(String teamName : teams) {
+			if(teamName.equalsIgnoreCase(team)) {
+				break;
+			}
+			i++;	
+		}		
+		return w[i];
+	}
 //	public              int losses(String team)                    // number of losses for given team
 //	public              int remaining(String team)                 // number of remaining games for given team
 //	public              int against(String team1, String team2)    // number of remaining games between team1 and team2
